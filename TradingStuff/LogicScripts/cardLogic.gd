69c6b8748d045f9743c249c985ad60ecb9ee6a64
Касик Максим Олегович <maxxka@outlook.com>
2025-04-16 22:26:53 +0300

    json card added, midwork commit 2

--- TradingStuff/LogicScripts/cardLogic.gd
+++ TradingStuff/LogicScripts/cardLogic.gd
@@ -4,11 +4,39 @@ class_name Card
 
 var name: String
 var base_price: int
+var rarity: int
+
+var str: int
+var def: int
+var mag: int
+
+var series: String
+var picture: Resource
+
+func get_literal_name() -> String:
+	match rarity:
+		0:
+			return "C"
+		1:
+			return "U"
+		2:
+			return "R"
+		3:
+			return "SR"
+		4:
+			return "SP"
+		5:
+			return "SSP"
+		_:
+			return "NO"
 
 func _init(name: String, base_price: int):
 	self.name = name
 	self.base_price = base_price
 
-func get_price() -> int:
+func get_price(coeffVector: Vector3i) -> int:
 	# Пока просто возвращает базовую цену
-	return base_price
+	var sum = int(Vector3(coeffVector).dot(Vector3(str, def, mag)))
+	sum += (rarity+1) * randf_range(0.5, 1.5) * 20
+	sum *= randf_range(0.8, 1.2)
+	return sum
--- globals.gd
+++ globals.gd
@@ -4,3 +4,12 @@ var my_collection = [Card.new("Spider", 10), Card.new("Tarakan", 15)]
 var playerAccount : Player = Player.new("Joe", 42, my_collection, preload("res://TradingStuff/Style/Portraits/M3.png"))
 
 var traderList : Array[Trader]
+var cardList: Array[Card]
+
+
+func _ready() -> void:
+	pass
+	
+func create_cards -> void:
+	
+	
